d58b4d5d2d8c28c15d248e04ee903a043e1d6526
AreebHanas <arbofficial02@gmail.com>
2024-01-01 15:36:12 +0530

    API routes

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,10 @@
+import User from "../models/user.model.js";
+
+export const fetch = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    console.log("Cannot fetch data : ", error);
+  }
+};
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import router from "./routes/user.route.js";
 
 const app = express();
 dotenv.config();
@@ -17,3 +18,5 @@ const db = mongoose
   .catch((error) => {
     console.log("Can not connect to the data base ", error);
   });
+
+app.use("/user", router);
